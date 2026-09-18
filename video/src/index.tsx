5d9976fe4fc09b08aef4a95d9648c33279c9cb8b
Akunimal <inti_gesell@hotmail.com>
2026-09-18 18:08:21 -0300

    feat(video): add narration track to remotion demo

--- video/src/index.tsx
+++ video/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Composition, registerRoot } from 'remotion';
+import { Audio, Composition, registerRoot, staticFile } from 'remotion';
 import { NoahDemo, DURATION_IN_FRAMES, FPS } from './NoahDemo';
+
+const NoahDemoWithNarration: React.FC = () => (
+  <>
+    <NoahDemo />
+    <Audio src={staticFile('noah-narration.wav')} volume={1} />
+  </>
+);
 
 export const RemotionRoot: React.FC = () => (
   <Composition
     id="NoahDemo"
-    component={NoahDemo}
+    component={NoahDemoWithNarration}
     durationInFrames={DURATION_IN_FRAMES}
     fps={FPS}
     width={1920}
